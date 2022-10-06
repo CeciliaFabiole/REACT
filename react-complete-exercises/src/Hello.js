6857d8ebdf3071cc5bc90a36cf7a1e25136285fa
ceci <ceciliafabiole18@gmail.com>
2022-10-06 13:07:36 +0300

    feat: add components-02

---
+++ react-complete-exercises/src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//CLASS COMPONENT
+// export class Hello extends React.Component {
+//     render() {
+//         return <h1>Hello, World!</h1>
+//     }
+// }
+
+//FUNCTION COMPONENT
+export function Hello(){
+    return <h1>Hello,World!</h1>
+}
