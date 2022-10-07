f3d819fabe131dccc7c9d49fddd09cdfb5301b49
ceci <ceciliafabiole18@gmail.com>
2022-10-07 16:59:30 +0300

    feat:add lists-06

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ Esercizi React
 - lists-03 Create a TodoList component that renders a ul tag with a li tag for each item contained in the items state variable. The items state variable should be an array of strings. The TodoList component should also contain an input tag and a button. When the button is clicked, the event handler should add the value of the input tag to the items array.
 - lists-04 Modify the TodoList component so that the input clears every time a Todo is added to the items array.
 - lists-05 Modify the TodoList by adding a "reset" button that clears the items array when clicked.
-- lists-06
+- lists-06 Modify the TodoList by adding a "remove" button to each li tag. When clicked, the event handler should remove corresponding item from the items array.
 - styling-components-01
 - styling-components-03
 
--- react-complete-exercises/src/TodoList.js
+++ react-complete-exercises/src/TodoList.js
@@ -19,18 +19,25 @@ import React, { useState } from "react"
 //             return {items : items, newItem: ''}
 //         })
 //     }
-//      resetItem = ()=>{
-//          this.setState(()=> {
-//              let items = this.state.items
-//              items.length = 0
-//              // items.pop() per cancellarne uno alla volta
-//              return {items:items, newItem: ''}
-//          })
-//      }
+//     resetItem = ()=>{
+//         this.setState(()=> {
+//             let items = this.state.items
+//             items.length = 0
+//             // items.pop() per cancellarne uno alla volta
+//             return {items:items, newItem: ''}
+//         })
+//     }
+//     removeItem = (index)=>{
+//         this.setState(()=>{
+//             let items=this.state.items
+//             items.splice(index, 1)
+//             return {items:items}
+//         })
+//     }
 //     render() {
 //         return (
 //             <div>
-//                 <ul>{this.state.items.map((item, index) => <li key={index}>{item}</li>)}</ul>
+//                 <ul>{this.state.items.map((item, index) => <li key={index}>{item}<button onClick={()=>this.removeItem(index)}>x</button></li>)}</ul>
 //                 <input name="newItem" value={this.state.newItem} onChange={(e)=> this.saveValue(e)}></input>
 //                 <button onClick={() => this.addItem()}>Add</button>
 //                 <button onClick={()=> this.resetItem()}>Reset</button>
@@ -49,7 +56,7 @@ export function TodoList(){
     }
 
     function addItem(){
-        setItems(()=>[...items, newItem,])
+        setItems(()=>[...items, newItem])
         setNewItem('')
     }
 
@@ -57,10 +64,18 @@ export function TodoList(){
         setItems([])
     }
 
+    function removeItem(index){
+        let list = [...items]
+        list.splice(index,1)
+        setItems(()=>{
+            return [...list]
+        })
+    }
+
     return(
         <div>
             <h3>Todo List</h3>
-            <ul>{items.map((item, index) => <li key={index}>{item}</li>)}</ul>
+            <ul>{items.map((item, index) => <li key={index}>{item}<button onClick={()=>removeItem(index)}>x</button></li>)}</ul>
             <input name='newItem' value={newItem} onChange={(e)=>saveValue(e)}/>
             <button onClick={()=>addItem()}>Add</button>
             <button onClick={()=>clearItem()}>Reset</button>
