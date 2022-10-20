f73d533305adc775dbd7f7485325601af7ab0509
ceci <ceciliafabiole18@gmail.com>
2022-10-20 11:04:15 +0300

    feat: add swr-02

--- react-complete-exercises/src/useGithubUser.js
+++ react-complete-exercises/src/useGithubUser.js
@@ -1,12 +1,29 @@
 import useSWR from "swr"
+import shouldFetch from 'swr'
 
-const fetcher = url => fetch(url).then(response=>response.json())
+const fetcher = url => fetch(url).then((response)=>{
+    if (response.status !== 200){
+        const error = new Error('An error occurred while fetching the data.')
+        throw error
+    }
+    return response.json()
+})
+// const fetcher = async url => {
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//       const error = new Error('An error occurred while fetching the data.')
+//       throw error
+//     }
+//     return res.json()
+// }
 
 export function useGithubUser(username){
-    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const {data, error} = useSWR(shouldFetch ? `https://api.github.com/users/${username}` : null, fetcher)
+    // Differenza?
+    // const {data, error} = useSWR(() => shouldFetch ? `https://api.github.com/users/${username}` : null, fetcher)
     return{
         user: data,
         loading: !error && !data,
-        error: error,
+        error,
     }
 }
